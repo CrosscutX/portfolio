5b2de1147827fe7220fb952923ea66559d221fcd
CrosscutX <crosscutcode@gmail.com>
2023-03-12 19:47:31 -0400

    Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import React from "react";
 import "./style.css";
 import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
-      <Hero></Hero>
+      <Hero />
+      <Skills />
+      <AboutMe />
+      <Projects />
+      <Footer />
     </>
   );
 }
