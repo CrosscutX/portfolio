b7d2a3310b0269aa74f4a5ee79ee3a057a01e499
CrosscutX <crosscutcode@gmail.com>
2023-03-14 15:38:20 -0400

    Mobile adjustments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,20 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="hero colored-container">
-      <h1>
-        Hey, I'm <span className="alex">Alex</span>
-      </h1>
-      <h2>
-        I'm a <span className="emphasis">Front-End Developer</span> based out of{" "}
-        <span className="emphasis">Ohio</span>
-      </h2>
-      <p>
-        I love bringing bringing websites to life with tools like{" "}
-        <span className="emphasis react">React</span>
-      </p>
-      <p className="emphasis react">donahue.alex@gmail.com</p>
+      <div className="hero-text-container">
+        <h1>
+          Hey, I'm <span className="alex">Alex Donahue</span>
+        </h1>
+        <h2>
+          I'm a <span className="emphasis">Front-End Developer</span> based out
+          of <span className="emphasis">Ohio</span>
+        </h2>
+        <p>
+          I love bringing bringing websites to life with tools like{" "}
+          <span className="emphasis react">React</span>
+        </p>
+        <p className="emphasis react">donahue.alex@gmail.com</p>
+      </div>
     </div>
   );
 }
