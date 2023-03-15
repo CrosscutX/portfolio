3b2ddace24698ea7e60da04bc5cd7ef7e370375b
CrosscutX <crosscutcode@gmail.com>
2023-03-15 16:39:55 -0400

    Added project screenshots and setup ProjectItem

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function ProjectItem() {
-  return <div className="project-item">ProjectItem</div>;
+export default function ProjectItem(props) {
+  return (
+    <div className="project-item">
+      <img src="" alt="" />
+    </div>
+  );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import Battleship from "../assets/battleship.png";
+import Quiz from "../assets/quizzical.png";
 
 export default function Projects() {
+  function descriptionHelper() {}
+
   return (
     <div className="projects">
-      <ProjectItem />
-      <ProjectItem />
+      <ProjectItem
+        image={Battleship}
+        title="Battleship"
+        description={descriptionHelper("battleship")}
+        live={null}
+        github={null}
+      />
+      <ProjectItem
+        image={Quiz}
+        title="Quizzical"
+        description={descriptionHelper("quizzical")}
+        live={null}
+        github={null}
+      />
     </div>
   );
 }
