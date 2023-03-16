a0d32d95192e0c72784fc3a826cbdf9101b17936
CrosscutX <crosscutcode@gmail.com>
2023-03-15 23:04:57 -0400

    Project section contains most features

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,7 +3,17 @@ import React from "react";
 export default function ProjectItem(props) {
   return (
     <div className="project-item">
-      <img src={props.image} alt={props.title} />
+      <img src={props.image} alt={props.text} />
+      <div className="project-text">
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+        <div className="button-container">
+          <a className="link-button" href="google.com">
+            Live
+          </a>
+          <a className="link-button">Github</a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,11 +7,25 @@ export default function Projects() {
   //helper function used to keep my ProjectItem components more clean
   function descriptionHelper(project) {
     if (project === "battleship") {
-      return;
+      return "A working imitation of battleship! This project was instrumental in developing my ability to problem solve using javascript. Figuring out the logic was no easy feat! Working through the process of selecting things and using event listeners for something of this scope was eye opening, and really led to me growing an appreciation for how React simplifies these things.";
     }
 
     if (project === "quizzical") {
-      return;
+      const link = (
+        <a href="https://opentdb.com/api_config.php" className="link">
+          here
+        </a>
+      );
+      return (
+        <>
+          A simple quiz app using the open trivia api {link}. This app makes a
+          call to an api and then displays said questions from the api and
+          allows you to answer them and then check said answers. While working
+          on this I was able to further hone my skills with React, and I was
+          able to play around with a lot of new concepts that I had learned,
+          such as hooks and conditional rendering.
+        </>
+      );
     }
 
     if (project === "game recommender") {
@@ -26,6 +40,8 @@ export default function Projects() {
         <h1>Projects</h1>
       </div>
       <div className="projects-container">
+        <ProjectItem title="Game Recommender" />
+
         <ProjectItem
           image={Battleship}
           title="Battleship"
