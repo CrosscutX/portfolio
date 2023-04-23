bfea4b7359e078b256118a5271b1d8cdfd2f0f5d
CrosscutX <crosscutcode@gmail.com>
2023-04-23 16:24:00 -0400

    Movie Watch List added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,16 @@ export default function Projects() {
   //helper function used to keep my ProjectItem components more clean
   function descriptionHelper(project) {
     if (project === "battleship") {
-      return "A working imitation of battleship! This project was instrumental in developing my ability to problem solve using javascript. Figuring out the logic was no easy feat! Working through the process of selecting things and using event listeners for something of this scope was eye opening, and really led to me growing an appreciation for how React simplifies these things.";
+      return (
+        <>
+          A working imitation of battleship! This project was instrumental in
+          developing my ability to problem solve using javascript. Figuring out
+          the logic was no easy feat! Working through the process of selecting
+          things and using event listeners for something of this scope was eye
+          opening, and really led to me growing an appreciation for how React
+          simplifies these things.
+        </>
+      );
     }
 
     if (project === "quizzical") {
@@ -30,7 +39,21 @@ export default function Projects() {
     }
 
     if (project === "Movie Watch List") {
-      return;
+      const matchaLink = (
+        <a href="https://github.com/MatchaSage" className="link">
+          MatchaSage
+        </a>
+      );
+      return (
+        <>
+          Project that I created with my friend and fellow learner {matchaLink}.
+          For this project we created a react app capable of fetching data from
+          a movie api and allowed users to store that information on their
+          machine using local storage. During this project I learned a lot about
+          the general workflow of React, and I got a lot of good practice using
+          git with another person on a team.
+        </>
+      );
     }
     return;
   }
@@ -41,7 +64,13 @@ export default function Projects() {
         <h1>Projects</h1>
       </div>
       <div className="projects-container">
-        <ProjectItem image={MoveList} title="Movie Watch List" />
+        <ProjectItem
+          image={MoveList}
+          title="Movie Watch List"
+          description={descriptionHelper("Movie Watch List")}
+          live={"https://rococo-crostata-bb9989.netlify.app"}
+          github={"https://github.com/MatchaSage/movie-list-"}
+        />
 
         <ProjectItem
           image={Battleship}
